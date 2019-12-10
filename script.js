5b67aba5787c6c75584fe0c62b911b0bfe751fe7
2david2 <54052498+2david2@users.noreply.github.com>
2019-12-10 17:04:51 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function tap(clicked_id) {
         document.getElementById(clicked_id).style.backgroundImage="url(krestik.jpg)"
         player.push(clicked_id)
         ai()
+        endGame()
     }
 
 }
@@ -20,22 +21,33 @@ function ai() {
             document.getElementById(id).style.backgroundImage = "url(nolik.jpg)"
             aiMoves.push(id)
             break
+            endGame()
         }
-    }
-
-    function endGame() {
-        if (player.includes(1) & player.includes(2) & player.includes(3) &
-            player.includes(4) & player.includes(5) & player.includes(6) &
-            player.includes(1) & player.includes(4) & player.includes(7) &
-            player.includes(2) & player.includes(5) & player.includes(8) &
-            player.includes(3) & player.includes(6) & player.includes(9) &
-            player.includes(1) & player.includes(5) & player.includes(9) &
-            player.includes(3) & player.includes(5) & player.includes(7))
-            alert("you win!!!")
     }
 }
 
-
+    function endGame() {
+        if (player.includes("1") & player.includes("2") & player.includes("3") |
+            player.includes("4") & player.includes("5") & player.includes("6") |
+            player.includes("7") & player.includes("8") & player.includes("9") |
+            player.includes("1") & player.includes("4") & player.includes("7") |
+            player.includes("2") & player.includes("5") & player.includes("8") |
+            player.includes("3") & player.includes("6") & player.includes("9") |
+            player.includes("1") & player.includes("5") & player.includes("9") |
+            player.includes("3") & player.includes("5") & player.includes("7")) {
+            alert("you win!!!")
+        }
+        else if (aiMoves.includes("1") & aiMoves.includes("2") & aiMoves.includes("3") |
+            aiMoves.includes("4") & aiMoves.includes("5") & aiMoves.includes("6") |
+            aiMoves.includes("7") & aiMoves.includes("8") & aiMoves.includes("9") |
+            aiMoves.includes("1") & aiMoves.includes("4") & aiMoves.includes("7") |
+            aiMoves.includes("2") & aiMoves.includes("5") & aiMoves.includes("8") |
+            aiMoves.includes("3") & aiMoves.includes("6") & aiMoves.includes("9") |
+            aiMoves.includes("1") & aiMoves.includes("5") & aiMoves.includes("9") |
+            aiMoves.includes("3") & aiMoves.includes("5") & aiMoves.includes("7")) {
+            alert("you lose!!!")
+        }
+    }
 
 
 
